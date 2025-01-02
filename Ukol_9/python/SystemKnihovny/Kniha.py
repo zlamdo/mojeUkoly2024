49812ff8cc11039ee71c72489d14d5708e81a67b
zlamdo <zlamdo@seznam.cz>
2025-01-02 12:15:35 +0000

    Ukol9

--- Ukol_9/python/SystemKnihovny/Kniha.py
+++ Ukol_9/python/SystemKnihovny/Kniha.py
@@ -5,7 +5,27 @@ class Kniha:
         self._rok_vydani = rok_vydani
         self.isbn = isbn
 
-    # doplňte potřebné gettry a setry
+    @property
+    def nazev(self) -> str:
+        return self._nazev
+
+    @property
+    def autor(self) -> str:
+        return self._autor
+
+    @property
+    def rok_vydani(self) -> int:
+        return self._rok_vydani
+
+    @property
+    def isbn(self) -> str:
+        return self._isbn
+
+    @isbn.setter
+    def isbn(self, value: str):
+        if len(value) != 13 or not value.isdigit():
+            raise ValueError("isbn musi mit 13 cislic")
+        self._isbn = value
 
     def __str__(self) -> str:
-        return ""
+        return f"nazev: {self._nazev} autor: {self._autor}, rok vydani: {self._rok_vydani}, isbn: {self._isbn}"
